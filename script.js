ee8c2acb9cec0d445374f1f923b1d585057ec881
mohit <54810032+mohitgupta-16@users.noreply.github.com>
2023-03-14 15:12:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const output = documen.querySelector("#output");
+
+function func() {
+	return Promise(resolve) {
+		setTimeout(() => {
+			resolve("Hello,world!");
+		}, 1000);
+	}
+}
+
+output.textContent = func();
